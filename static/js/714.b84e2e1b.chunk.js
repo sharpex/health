7fa72f6111a7170d071229b893955b38d3b6861c
techsystems <samwelmwangi1995@gmail.com>
2023-12-22 03:03:40 +0300

    'f'

---
+++ static/js/714.b84e2e1b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkhealthtechsystem=self.webpackChunkhealthtechsystem||[]).push([[714],{2714:function(e,s,t){t.r(s);t(2791);var h=t(8920),n=t(3252),c=t(184);s.default=function(){return(0,c.jsxs)("div",{children:[(0,c.jsx)(h.Z,{}),(0,c.jsx)("div",{style:{paddingTop:"40px"},children:(0,c.jsx)(n.Z,{})})]})}}}]);
+//# sourceMappingURL=714.b84e2e1b.chunk.js.map
---
+++ static/js/91.14eb2b72.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkhealthtechsystem=self.webpackChunkhealthtechsystem||[]).push([[91],{6091:function(e,s,t){t.r(s);t(2791);var h=t(8920),n=t(333),c=t(184);s.default=function(){return(0,c.jsxs)("div",{children:[(0,c.jsx)(h.Z,{}),(0,c.jsx)("div",{style:{paddingTop:"40px"},children:(0,c.jsx)(n.Z,{})})]})}}}]);
+//# sourceMappingURL=91.14eb2b72.chunk.js.map
